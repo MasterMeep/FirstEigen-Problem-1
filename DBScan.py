7852619da8cbbbf324b94b19fc002edd67940c09
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-07-08 20:12:38 -0500

    Update DBScan.py

--- DBScan.py
+++ DBScan.py
@@ -1,33 +1,31 @@
 import numpy as np
 
 from sklearn.cluster import DBSCAN
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 from sklearn import metrics
 from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
-
-"""centers = [[1, 1], [-1, -1], [1, -1]]
-X, labels_true = make_blobs(
-    n_samples=750, centers=centers, cluster_std=0.4, random_state=0
-)
-
-X = StandardScaler().fit_transform(X)"""
+from matplotlib import pyplot as plt
+import time
+from sklearn.neighbors import NearestNeighbors
+import pandas as pd
+import plotly.graph_objs as go
 
 
+import pandas_helper_calc
 
+df = pd.read_csv('loans.csv') #data to be read
 
-X, y = make_blobs(n_samples = 10000,
-                  # two feature variables,
-                  n_features = 5,
-                  # three clusters,
-                  centers = 5,
-                  # with .5 cluster standard deviation,
-                  cluster_std = 0.4,
-                  # shuffled,
-                  shuffle = True)
+#get only the dimensions you need. IMPORTANT: do not use data that classifies someone or strings, such as eye color, dates, education etc
+X = df.drop(['Loan_ID', 'effective_date', 'due_date', 'paid_off_time', 'past_due_days', 'education', 'loan_status', 'Gender'], axis=1)
+X = X[:299]
 
+#min samples is minimum amount of samples for a cluster to be created, currently set to double the feature variables
+min_samples = X.shape[1]*2
 
-
-db = DBSCAN(eps=0.5, min_samples=10).fit(X)
+#db scan implementation, only change eps here, change it with the code at the very bottom
+db = DBSCAN(eps=3, min_samples=min_samples).fit(X)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
@@ -35,50 +33,49 @@ labels = db.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
 
+
 print("Estimated number of clusters: %d" % n_clusters_)
 print("Estimated number of noise points: %d" % n_noise_)
-import matplotlib.pyplot as plt
 
-# Black removed and is used for noise instead.
-unique_labels = set(labels)
-colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
+index = [i for i, x in enumerate(list(labels)) if x == -1]
 
-    class_member_mask = labels == k
+#print anomalous data points
+print(df.iloc[[*index]])
 
-    xy = X[class_member_mask & core_samples_mask]
-    plt.plot(
-        xy[:, 0],
-        xy[:, 1],
-        "o",
-        markerfacecolor=tuple(col),
-        markeredgecolor="k",
-        markersize=14,
-    )
+#if the dimensions is greater than 2 decrese to 2, this is only for viewing purposes
+if X.shape[1] > 2:
+    X_tsne = pd.DataFrame(TSNE(n_components=2, random_state=0).fit_transform(X))
 
-    xy = X[class_member_mask & ~core_samples_mask]
-    plt.plot(
-        xy[:, 0],
-        xy[:, 1],
-        "o",
-        markerfacecolor=tuple(col),
-        markeredgecolor="k",
-        markersize=6,
-    )
-
-plt.title("Estimated number of clusters: %d" % n_clusters_)
+#show the plot with all datapoints and coloured clusters
+plt.scatter(X_tsne.iloc[:,0], X_tsne.iloc[:,1], c=labels, cmap='rainbow')
+plt.colorbar()
+plt.title("DBSCAN")
 plt.show()
 
-def predict(db, x):
-  dists = np.sqrt(np.sum((db.components_ - x)**2, axis=1))
-  i = np.argmin(dists)
-  return db.labels_[db.core_sample_indices_[i]] if dists[i] < db.eps else -1
 
-X_test = [[2,-2],[10,3],[1,-6]]
+from sklearn.cluster import KMeans
 
-for i in range(len(X_test)):
-   print('test point: {}, predicted label: {}'.format(X_test[i], 
-                                               predict(db, X_test[i])))
+#implement k-means
+kmeans = KMeans(n_clusters=n_clusters_, random_state=0).fit(X)
+
+#predict for data point(s), to add more add more rows to the pd dataframe ex: {0:[1000, 10],1:[15, 10],2:[74, 10]}, currently using test data
+prediction = kmeans.predict(pd.DataFrame({0:[1000],1:[15],2:[74]}))
+#k-means anomaly is group 0 while db scan is -1, so just subtract 1 to get the db scan group that you can see in the plot
+print(prediction-1)
+
+#show plot again. simply for viewing purposes
+plt.scatter(X_tsne.iloc[:,0], X_tsne.iloc[:,1], c=labels, cmap='rainbow')
+plt.colorbar()
+plt.title("DBSCAN")
+plt.show()
+
+
+#uncomment this code to find the optimal eps value for db scan, when you do you will get a graph, and find the point with maximum curvature, set eps to the y value of that point, or position on line
+"""neigh = NearestNeighbors(n_neighbors=2)
+nbrs = neigh.fit(X)
+distances, indices = nbrs.kneighbors(X)
+distLen = len(distances)
+distances = np.sort(distances, axis=0)
+distances = distances[:,1]
+plt.plot(distances)
+plt.show()"""
